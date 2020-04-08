ceeee3b1cd766afc19906eb2b48669fd832f98f8
ukishere <61353722+ukishere@users.noreply.github.com>
2020-04-08 21:53:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+documents = [
+    {"type": "Passport", "number": "2207 876234", "name": "Alexey"},
+    {"type": "Invoice", "number": "11-2", "name": "Alexander"},
+    {"type": "Insurance", "number": "10006", "name": "Petr"},
+    {"type": "Passport", "number": "5455 028765", "name": "Dmitry"},
+    {"type": "Passport", "number": "5400 028765", "name": "Alena"},
+    {"type": "Passport", "number": "5455 002299", "name": "Vasilisa"},
+    {"type": "Invoice", "number": "14-4"}
+]
+
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006', '5400 028765', '5455 002299'],
+    '3': ['14-4', '14-5']
+}
+
+def print_all_names_by_documents():
+    found = False
+    print('Имена всех владельцев документов, хранящихся в архиве:')
+    for directory in directories.values():
+        for each_document in directory:
+            found = False
+            for document in documents:
+                try:
+                    if document["number"] == each_document:
+                        print(f'{document["name"]} - владелец документа {each_document}')
+                        found = True
+                except KeyError:
+                    print(f'У документа {each_document} не указан владелец')
+                    found = True
+            if not found:
+                print(f'Для документа {each_document} отсутствует запись')
+                found = True
+
+while True:
+    input_command = input("Введите команду: ")
+
+    if input_command == "sc":
+        print_all_names_by_documents()
+    elif input_command == "q":
+        break
+
+    else:
+        print("Неверный ввод. Доступные команды: sc и q.")
